7bcfc41ea9ebbd29272269737336cabae83c5468
Briano Santos <brianosantos@gmail.com>
2016-08-31 17:07:25 -0700

    created 5 outlets and 2 actions and connected them to the storyboard.

--- Tapper2/Tapper2/ViewController.swift
+++ Tapper2/Tapper2/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    
+    
+    //Outlets
+    @IBOutlet weak var tapperLogo: UIImageView!
+    @IBOutlet weak var howManyTxtField: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var coinBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onPlayBtnPressed(sender: AnyObject) {
+        tapperLogo.hidden = true
+        howManyTxtField.hidden = true
+        playBtn.hidden = true
+        
+        coinBtn.hidden = false
+        tapsLbl.hidden = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onCoinBtnPressed(sender: AnyObject) {
+        
     }
-
 
 }
 
